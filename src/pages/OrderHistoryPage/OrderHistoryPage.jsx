fe93f1d16e739673ed197f422ea70b0f4149d7d2
BUSAMT <amtulrb@gmail.com>
2023-09-07 14:58:47 +1000

    order list functionality started

--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,7 +1,49 @@
-export default function OrderHistoryPage() {
+import { useState, useEffect, useRef } from 'react';
+import * as itemsAPI from '../../utilities/items-api';
+import * as ordersAPI from '../../utilities/orders-api';
+// import './NewOrderPage.css';
+import { Link, useNavigate } from 'react-router-dom';
+import Logo from '../../components/Logo/Logo';
+import OrderDetail from '../../components/OrderDetail/OrderDetail';
+import UserLogOut from '../../components/UserLogOut/UserLogOut';
+import OrderList from '../../components/OrderList/OrderList'
+
+export default function OrderHistoryPage({ user, setUser }) {
+  const [orders, setOrders] = useState(null)
+  const [cart, setCart] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(function() {
+    async function getOrders() {
+      const orders = await ordersAPI.getAll();
+
+    }
+  }, [])
+
+  async function handleChangeQty(itemId, newQty) {
+    const updatedCart = await ordersAPI.setItemQtyInCart(itemId, newQty);
+    setCart(updatedCart);
+  }
+
+  async function handleCheckout() {
+    await ordersAPI.checkout();
+    navigate('/orders');
+  }
+
+  
   return (
-    <>
-      <h1>OrderHistoryPage</h1>
-    </>
+    <main className="NewOrderPage">
+      <aside>
+        <Logo />
+        <Link to="/orders/new" className="button btn-sm">NEW ORDER</Link>
+        <UserLogOut user={user} setUser={setUser} />
+      </aside>
+      <OrderList />
+      <OrderDetail
+        order={cart}
+        handleChangeQty={handleChangeQty}
+        handleCheckout={handleCheckout}
+      />
+    </main>
   );
 }
